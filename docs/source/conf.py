29af9efebb384278782676182568c2ab618f3ec9
Alix Chagué <alix.chague@inria.fr>
2022-08-09 17:03:25 +0200

    changed config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Tutoriel eScriptorium'
+copyright = '2022'
+author = 'Alix Chagué'
 
 release = '0.1'
 version = '0.1.0'
